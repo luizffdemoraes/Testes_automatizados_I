1cc3aa6e7dcaacd7f1f9b0053ff3e1e44ddcda6a
Luiz Moraes <lffm1994@gmail.com>
2021-06-09 00:12:17 -0300

    Primeiro teste na prática com JUnit

---
+++ aula-junit/src/tests/entities/AccountTests.java
@@ -0,0 +1,35 @@
+package tests.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Account;
+
+public class AccountTests {
+	
+	//deposito deveria aumentar o saldo quando eu entrar com um valor positivo de dinheiro
+	@Test
+	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+		
+		/* 
+		 * Padrão AAA
+		 * -> Arrange - instancie os objetos necessários;
+		 * -> Act - execute as ações necessárias;
+		 * -> Assert - declare o que deveria acontecer (resultado esperado)
+		 * ---------------------------------------------------------------------
+		 * -> Assertions - biblioteca de declarações ele possui metodos estaticos
+		 */
+	
+		// - Arrange
+		double amount = 200.0;
+		double expectedValue = 196.0;
+		Account acc = new Account(1L, 0.0);
+		
+		// - Act
+		acc.deposit(amount);
+		
+		// - Assert
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+
+}
