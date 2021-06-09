2caea72635094806d57df56462c9f66859f7e654
Luiz Moraes <lffm1994@gmail.com>
2021-06-09 01:31:53 -0300

    Teste saque total deveria limpar saldo e retornar todo saldo

--- aula-junit/src/tests/entities/AccountTests.java
+++ aula-junit/src/tests/entities/AccountTests.java
@@ -46,5 +46,21 @@ public class AccountTests {
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 		
 	}
+	
+	//terceiro teste - chamar o metodo que deveria zerar o saldo
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+		
+		double expectedValue = 0.0;
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		
+		 double result = acc.fullWithdraw();
+		
+//		Assertions.assertEquals(expectedValue, acc.getBalance());
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
+		
+	}
 
 }
